413df34e015463fc970914c4fe71ea59c3261e37
yudharizkiiii-ux <yudharizkiiii@gmail.com>
2026-07-27 15:26:12 +0700

    chore: add push.bat script

---
+++ push.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Pushing local main branch to git@github.com:yudharizkiiii-ux/simpanpinjamkoperasi.git...
+git push git@github.com:yudharizkiiii-ux/simpanpinjamkoperasi.git main
+echo.
+echo Selesai!
+pause
