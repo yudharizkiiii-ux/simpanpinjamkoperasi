d465a637c8173b2c7a3d224980e4e7f1985e945e
yudharizkiiii-ux <yudharizkiiii@gmail.com>
2026-07-27 15:35:49 +0700

    feat: Add Capacitor mobile android configuration

---
+++ android/app/src/main/java/com/koperasi/simpanpinjam/MainActivity.java
@@ -0,0 +1,5 @@
+package com.koperasi.simpanpinjam;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.koperasi.simpanpinjam',
+  appName: 'Simpan Pinjam',
+  webDir: 'dist'
+};
+
+export default config;
